b3b9d7083f64ff14f6dc70cc159c33b47ecb564b
niclas <niclas@hedhman.org>
2019-11-30 13:07:20 +0800

    Added #includes in the umbrella header file.

--- src/Demiurge.h
+++ src/Demiurge.h
@@ -5,5 +5,14 @@
 #include "AudioInPort.h"
 #include "AudioOutPort.h"
 #include "CvInPort.h"
+#include "GatePort.h"
+#include "Mixer.h"
+#include "Pan.h"
+#include "Potentiometer.h"
+#include "PushButton.h"
+#include "Signal.h"
+#include "SoundProcessor.h"
+#include "Threshold.h"
+#include "VCO.h"
 
 #endif
